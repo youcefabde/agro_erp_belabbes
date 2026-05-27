918dda62d0ff4c93fb40857125243dab1e26b12b
abdesamed <youcefwawa3@gmail.com>
2026-05-27 11:30:39 +0100

    clean final index

--- index.js
+++ index.js
@@ -1,177 +1,105 @@
 const express = require("express");
 const path = require("path");
 const db = require("./db");
-const app = express();
-app.use(express.static("public"));
-const apiRoutes = require("./routes/apiRoutes");
-app.use("/", apiRoutes);
-
-const bcrypt = require("bcrypt");
 const session = require("express-session");
 
+const app = express();
 
+/* Routes */
+const apiRoutes = require("./routes/apiRoutes");
+const authRoutes = require("./routes/authRoutes");
+const clientRoutes = require("./routes/clientRoutes");
+const produitRoutes = require("./routes/produitRoutes");
+const factureRoutes = require("./routes/factureRoutes");
 
+/* Auth Middleware */
+const { isAuthenticated, isAdmin } = require("./middleware/auth");
 
-const {
-    isAuthenticated,
-    isAdmin
-} = require("./middleware/auth");
-const clientRoutes =
-require("./routes/clientRoutes");
-
-const produitRoutes =
-require("./routes/produitRoutes");
-
-const factureRoutes =
-require("./routes/factureRoutes");
-
-const authRoutes =
-require("./routes/authRoutes");
-
-
-function isModerator(req, res, next) {
-
-    if (req.session.user.role === "moderator") {
-
-        next();
-
-    } else {
-
-        res.send("Access Denied");
-
-    }
-
-}
-function isEmployee(req, res, next) {
-
-    if (req.session.user.role === "employee") {
-
-        next();
-
-    } else {
-
-        res.send("Access Denied");
-
-    }
-
-}
-
+/* Middlewares */
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
+
 app.use(session({
-
     secret: "secretkey",
-
     resave: false,
-
     saveUninitialized: false
-
 }));
 
+/* Routes */
 app.use("/", authRoutes);
+app.use("/", apiRoutes);
 app.use("/", clientRoutes);
 app.use("/", produitRoutes);
 app.use("/", factureRoutes);
 
+/* Role Middlewares */
+function isModerator(req, res, next) {
+    if (req.session.user?.role === "moderator") {
+        next();
+    } else {
+        res.send("Access Denied");
+    }
+}
 
+function isEmployee(req, res, next) {
+    if (req.session.user?.role === "employee") {
+        next();
+    } else {
+        res.send("Access Denied");
+    }
+}
 
-
+/* View Engine */
 app.set("view engine", "ejs");
 
-
+/* Admin Dashboard */
 app.get("/admin", isAuthenticated, isAdmin, (req, res) => {
 
-    const clientsSql =
-    "SELECT COUNT(*) AS totalClients FROM clients";
-
-    const produitsSql =
-    "SELECT COUNT(*) AS totalProduits FROM produits";
-
-    const facturesSql =
-    "SELECT COUNT(*) AS totalFactures FROM factures";
-
-    const revenueSql =
-    "SELECT SUM(montant) AS totalRevenue FROM factures";
+    const clientsSql = "SELECT COUNT(*) AS totalClients FROM clients";
+    const produitsSql = "SELECT COUNT(*) AS totalProduits FROM produits";
+    const facturesSql = "SELECT COUNT(*) AS totalFactures FROM factures";
+    const revenueSql = "SELECT SUM(montant) AS totalRevenue FROM factures";
 
     db.query(clientsSql, (err, clientsResult) => {
-
-        if (err) {
-            return res.send("Database Error");
-        }
+        if (err) return res.send("Database Error");
 
         db.query(produitsSql, (err, produitsResult) => {
-
-            if (err) {
-                return res.send("Database Error");
-            }
+            if (err) return res.send("Database Error");
 
             db.query(facturesSql, (err, facturesResult) => {
-
-                if (err) {
-                    return res.send("Database Error");
-                }
+                if (err) return res.send("Database Error");
 
                 db.query(revenueSql, (err, revenueResult) => {
-
-                    if (err) {
-                        return res.send("Database Error");
-                    }
+                    if (err) return res.send("Database Error");
 
                     res.render("admin", {
-
-                        totalClients:
-                        clientsResult[0].totalClients,
-
-                        totalProduits:
-                        produitsResult[0].totalProduits,
-
-                        totalFactures:
-                        facturesResult[0].totalFactures,
-
-                        totalRevenue:
-                        revenueResult[0].totalRevenue || 0
-
+                        totalClients: clientsResult[0].totalClients,
+                        totalProduits: produitsResult[0].totalProduits,
+                        totalFactures: facturesResult[0].totalFactures,
+                        totalRevenue: revenueResult[0].totalRevenue || 0
                     });
-
                 });
-
             });
-
         });
-
     });
-
 });
 
-
-app.get("/moderator",isAuthenticated,isModerator,(req, res) => {
-
-    res.sendFile(
-        path.join(__dirname,
-        "views",
-        "moderator.html")
-    );
-
-});
-app.get("/employee",isAuthenticated,isEmployee,(req, res) => {
-
-    res.sendFile(
-        path.join(__dirname,
-        "views",
-        "employee.html")
-    );
-
+/* Moderator */
+app.get("/moderator", isAuthenticated, isModerator, (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "moderator.html"));
 });
 
+/* Employee */
+app.get("/employee", isAuthenticated, isEmployee, (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "employee.html"));
+});
+
+/* Home */
 app.get("/", (req, res) => {
     res.send("Server is running 🚀");
 });
 
-
-
-
-
-
-
+/* Server */
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 });
