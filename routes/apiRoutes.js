7d2b408e0769e3b232fb81f4edb636dd1bf75fbb
abdesamed <youcefwawa3@gmail.com>
2026-05-27 10:09:13 +0100

    add home route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -74,4 +74,8 @@ router.get("/api/factures", (req, res) => {
     });
 
 });
+
+router.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "../views/login.html"));
+});
 module.exports = router;
