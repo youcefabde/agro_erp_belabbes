75f736488b6948407572e3b31eaa9f27b2342f54
abdesamed <youcefwawa3@gmail.com>
2026-05-27 15:32:57 +0100

    fix render port

--- index.js
+++ index.js
@@ -100,6 +100,11 @@ app.get("/", (req, res) => {
 });
 
 /* Server */
-app.listen(3000, () => {
+/*app.listen(3000, () => {
     console.log("Server running on port 3000");
+});*/
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
