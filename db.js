d6592494aad613f3ccd405e100f27060abeebf32
abdesamed <youcefwawa3@gmail.com>
2026-05-27 12:49:47 +0100

    fix database connection

--- db.js
+++ db.js
@@ -3,18 +3,19 @@ require("dotenv").config();
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: 3306
 });
 
 db.connect((err) => {
-    if (err) {
-        console.log("Database connection failed:", err);
-    } else {
-        console.log("MySQL Connected");
-    }
+  if (err) {
+    console.log("Database connection failed:", err);
+  } else {
+    console.log("MySQL Connected");
+  }
 });
 
 module.exports = db;
